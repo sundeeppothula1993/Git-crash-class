ead6f1859945f265c10858ef787c1f8c837bf3a5
sundeeppothula1993 <sundeeppothula@gmail.com>
2019-10-17 16:46:25 -0400

    First commit!

--- training.py
+++ training.py
@@ -1,6 +1,1 @@
 print('this is the training code!')
-print('Some random changes 1')
-print('Some random changes 2')
-print('Some random changes 3')
-print('Some random changes 4')
-print('Some random changes 5')
