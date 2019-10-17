989a865d0f5cfb6057311cd06c0db30ba05a0a70
sundeeppothula1993 <sundeeppothula@gmail.com>
2019-10-17 18:22:38 -0400

    Updgraded HT with Bayesian Optimization

--- training.py
+++ training.py
@@ -10,3 +10,13 @@ print('Some random changes 3')
 print('Some random changes 4')
 print('Some random changes 5')
 print('Added the Hyperparameter Tuning to the training code')
+
+print('Hyperparameter Tuning Bayesian Optimization')
+
+print('Some random changes 1')
+print('Some random changes 2')
+print('Some random changes 3')
+print('Some random changes 4')
+print('Some random changes 5')
+
+print('Hyperparameter Tuning Bayesian Optimization - Completed')
